597a012541d26773e527b4b85d07301b2a0009b1
hajricod <hajricod@gmail.com>
2026-03-07 15:05:33 +0400

    Update root route to return JSON response instead of view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['status' => 'online', 'version' => '1.0.0']);
 });
